bdadf68d77a47668b3c0e254cc923a8d7b3362d2
Dana Runge <7285068+mydana@users.noreply.github.com>
2025-04-20 01:27:36 -0700

    Clear

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -8,16 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var textField:
-        UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
